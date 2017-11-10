a0ae4e9ef43cd81f2e3cdb00b263cfe3f4ae83fb
Shade Wilson <shadew@uw.edu>
2017-11-09 21:36:14 -0800

    Implement basic scroll bar and detail views

--- VisitSeattle/SightsTableViewController.swift
+++ VisitSeattle/SightsTableViewController.swift
@@ -22,12 +22,7 @@ class SightsTableViewController: UITableViewController, VisitedDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
-        //NotificationCenter.default.addObserver(self, selector: #selector(loadList), name: NSNotification.Name(rawValue: "load"), object: nil)
+        tableView.showsVerticalScrollIndicator = true
         
     }
     // use with NotificationCenter
