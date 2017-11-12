a87a01f1f3c1cb01723f6369571295aa0a72e099
Shade Wilson <shadew@uw.edu>
2017-11-12 12:28:04 -0800

    Change weather segue to modal, create basic dismiss button

---
+++ VisitSeattle/Weather/WeatherViewController.swift
@@ -0,0 +1,31 @@
+//
+//  WeatherViewController.swift
+//  VisitSeattle
+//
+//  Created by Shade Wilson on 11/12/17.
+//  Copyright © 2017 Shade Wilson. All rights reserved.
+//
+
+import UIKit
+
+class WeatherViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func dismiss(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+
+   
+
+}
